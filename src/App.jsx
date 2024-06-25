ab4b33c90d552a901084122c5edcf56b15ede6d1
Maaz Hashmi <mhashmi373@gmail.com>
2024-06-25 19:34:12 -0400

    feat: completed education and experience sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import styles from "./App.module.css";
 import { About } from "./components/About/About";
 import { Contact } from "./components/Contact/Contact";
+import { Education } from "./components/Education/Education";
 import { Experience } from "./components/Experience/Experience";
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Education />
       <Experience />
       <Projects />
       <Contact />
---
+++ src/components/Education/Education.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+import styles from "./Education.module.css";
+import history from "../../data/education.json";
+import { getImageUrl } from "../../utils";
+
+export const Education = () => {
+    return (
+        <section className={styles.container} id="education">
+            <h2 className={styles.title}>Education</h2>
+            <div className={styles.content}>
+                <ul className={styles.history}>
+                    {history.map((historyItem, id) => {
+                        return (
+                            <li key={id} className={styles.historyItem}>
+                                <img
+                                    src={getImageUrl(historyItem.imageSrc)}
+                                    alt={`${historyItem.school} Logo`}
+                                />
+                                <div className={styles.historyItemDetails}>
+                                    <h3>{historyItem.school}</h3>
+                                    <h3>{historyItem.degree}</h3>
+                                    <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
+                                    <ul>
+                                        {historyItem.desc.map((item, id) => {
+                                            return <li key={id}>{item}</li>;
+                                        })}
+                                    </ul>
+                                </div>
+                            </li>
+                        );
+                    })}
+                </ul>
+                <div className={styles.skills}>
+                    <div className={styles.sideImgContainer}>
+                        <img src={getImageUrl("education/educationImg.png")} alt="Education" />
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    );
+};
