e121a6ea93b593f1e93846c3910d0529a2de1e43
Maaz Hashmi <mhashmi373@gmail.com>
2024-06-25 17:45:37 -0400

    feat: initial setup + hero section completed

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { SocialIcon } from 'react-social-icons'
+import { Typewriter } from 'react-simple-typewriter'
+
+import styles from "./Hero.module.css";
+import socials from "../../data/socials.json";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Maaz 👋</h1>
+        <p className={styles.description}> I'm
+          <Typewriter
+            words={[" a Developer 💻", " a Photographer 📸", " an Adventurer 🏞️", " a Music Lover 🎧", " a Sports Enthusiast 🏀"]}
+            loop={0}
+            cursor
+            cursorStyle='_'
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={1000}
+          />
+        </p>
+        <div className={styles.socialLinks}>
+          <SocialIcon style={{ width: "70px", height: "70px" }} url={socials.linkedin} target="_blank" />
+          <SocialIcon style={{ width: "70px", height: "70px" }} url={socials.github} target="_blank" />
+          <SocialIcon style={{ width: "70px", height: "70px" }} url={socials.instagram} target="_blank" />
+          <SocialIcon style={{ width: "70px", height: "70px" }} url={socials.twitter} target="_blank" />
+          <SocialIcon style={{ width: "70px", height: "70px" }} url={socials.facebook} target="_blank" />
+        </div>
+        <a href={socials.resume} className={styles.resumeBtn} target="_blank" >
+          Resume
+        </a>
+      </div>
+      <div className={styles.heroImgContainer}>
+        <img
+          src={getImageUrl("hero/hero.jpg")}
+          alt="Hero image of me"
+          className={styles.heroImg}
+        />
+      </div>
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section >
+  );
+};
