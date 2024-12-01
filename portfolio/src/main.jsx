03a5734234da1e79db776d242d1360fe000a4383
Maaz Hashmi <mhashmi373@gmail.com>
2024-12-01 16:31:29 -0500

    fix: removed unused roboto import

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -4,7 +4,6 @@ import App from "./App.jsx";
 import "./index.css";
 
 import "@fontsource/outfit";
-import "@fontsource/roboto";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
