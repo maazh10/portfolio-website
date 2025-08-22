cab7354915bfb3f11bb5fc102b23d24431a4cef1
Maaz Hashmi <mhashmi373@gmail.com>
2025-08-22 16:36:38 -0400

    fix: env based base URL for blog

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -6,6 +6,10 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://maazh.tech',
+	base: process.env.NODE_ENV === 'production' ? '/blog' : '',
+	build: {
+		assets: '_astro',
+	},
 	integrations: [
 		mdx(),
 		sitemap(),
