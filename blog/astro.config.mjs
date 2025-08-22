ef8c4f15364050c2ee7b0f1d0e4ca7f666f24df5
Maaz Hashmi <mhashmi373@gmail.com>
2025-08-22 19:19:34 -0400

    temp change accent color

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://maazh.tech',
-	base: '/blog',
+	base: process.env.NODE_ENV === 'production' ? '/blog' : '',
 	build: {
 		assets: '_astro',
 	},
