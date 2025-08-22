b1fa3dd0331cd48e732dd30f9137084f574e8b43
Maaz Hashmi <mhashmi373@gmail.com>
2025-08-22 19:01:25 -0400

    revert dockerfile

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://maazh.tech',
-	base: process.env.NODE_ENV === 'production' ? '/blog' : '',
+	base: '/blog',
 	build: {
 		assets: '_astro',
 	},
