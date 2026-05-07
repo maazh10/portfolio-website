d1345eb11ec01aeb99342675d0b9aa364f840c9f
Maaz Hashmi <mhashmi373@gmail.com>
2026-05-06 21:09:30 -0400

    chore: update meta data for OG

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "maaz h.";
-export const SITE_DESCRIPTION = "maaz's blog";
+export const SITE_DESCRIPTION = "I'm a software engineer and hobbyist photographer who enjoys building things and solving complex problems at scale. I strive to make positive impact in the world inside and outside of my career. In my free time, I like to explore the world and capture everything from landscapes to architecture through my lens.";
