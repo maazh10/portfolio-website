8f2f83fdf0b46cc9b3cbfed8395b82ac75775706
Maaz Hashmi <mhashmi373@gmail.com>
2026-05-09 00:21:14 -0400

    chore: update site description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "maaz h.";
-export const SITE_DESCRIPTION = "I'm a software engineer and hobbyist photographer who enjoys building things and solving complex problems at scale. I strive to make positive impact in the world inside and outside of my career. In my free time, I like to explore the world and capture everything from landscapes to architecture through my lens.";
+export const SITE_DESCRIPTION =
+  "I'm a software engineer and hobbyist photographer with a passion for building meaningful things, solving complex problems at scale, and exploring the world through photography. I care deeply about people and the planet, and strive to make a positive impact on the world both through my work and beyond it.";
