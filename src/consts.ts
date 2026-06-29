f94e18b3606f13be4efe3a593d37d0e7068c18b5
Maaz Hashmi <mhashmi373@gmail.com>
2026-06-28 21:02:46 -0400

    chore: update site description

--- src/consts.ts
+++ src/consts.ts
@@ -3,4 +3,4 @@
 
 export const SITE_TITLE = "maaz h.";
 export const SITE_DESCRIPTION =
-  "I'm a software engineer and hobbyist photographer with a passion for building meaningful things, solving complex problems at scale, and exploring the world through photography. I care deeply about people and the planet, and strive to make a positive impact on the world both through my work and beyond it.";
+  "engineer 💻 // photographer 📸 // adventurer 🏞️ // lifelong learner 📕";
